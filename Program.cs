032d23ea55aa82f19908a3ba233c9e02d82929ea
Kostya3151 <XMK5.05050306.019@gmail.com>
2019-01-03 20:11:01 +0200

    hello2

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задачки_ветвления_if___else__metanit_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ввидите значение:");
+          int a = Convert.ToInt32( Console.ReadLine());
+          int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"{a},{b}");
+           
+            Console.ReadKey();
+        }
+    }
+}
